7924ffe681f3be63a6e2440279216ab89a686b75
Leandro Heck <leoheck@gmail.com>
2021-11-01 18:33:37 -0300

    Improve initialization

--- initialize.sh
+++ initialize.sh
@@ -8,17 +8,34 @@
 # Install Github
 brew install git
 
+# Clone osx-setup scripts
+rm -rf ~/Documents/osx-setup
+git clone https://github.com/leoheck/osx-setup.git ~/Documents/osx-setup
+
+# Clean the shitty dock
+brew install dockutil
+dockutil --remove "Calendars"
+dockutil --remove "Contacts"
+dockutil --remove "FaceTime"
+dockutil --remove "Mail"
+dockutil --remove "Maps"
+dockutil --remove "Messages"
+dockutil --remove "Music"
+dockutil --remove "Notes"
+dockutil --remove "Photos"
+dockutil --remove "Podcasts"
+dockutil --remove "Reminders"
+dockutil --remove "TV"
+
 # Install OH-MY-ZSH (colors)
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # Spaceship theme
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 sed -i "" "s/robbyrussell/spaceship/g" ~/.zshrc
 
-# Clone osx-setup scripts
-rm -rf ~/Documents/osx-setup
-git clone https://github.com/leoheck/osx-setup.git ~/Documents/osx-setup
+zsh
 
 # Launch system settings to enable shit that cannot be enabled by script
-open -a /System/Applications/System\ Preferences.app
+#open -a /System/Applications/System\ Preferences.app
