bb216f6a23e2609f97087c1408f3f6c7f774305c
Leandro Heck <leoheck@gmail.com>
2021-11-06 11:38:29 -0300

    Update

--- assets/macos-info.sh
+++ assets/macos-info.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
 
-# Serial Number
-# Model
-# Model Year
-# Waranty?
-
-# Processor
-# Disk
-# Memory
-# Graphics card
-
 # serial number
 serial_number=$(ioreg -l | grep IOPlatformSerialNumber | cut -d= -f2 | sed -Ee 's/^[[:space:]]+//g' | sed "s/\"//g")
+
+# model
+model=(/usr/libexec/PlistBuddy -c "print :'CPU Names':$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}' | cut -c 9-)-en-US_US" ~/Library/Preferences/com.apple.SystemProfiler.plist)
+
+# year
+year=
 
 # processor
 processor=$(sysctl -a | grep machdep.cpu.brand_string | cut -d: -f2 | sed -Ee 's/^[[:space:]]+//g')
 
-# memory size
-memory_size=$(system_profiler SPHardwareDataType | grep "Memory:" | cut -d: -f2 | sed -Ee 's/^[[:space:]]+//g')
+# CPUS
+n_cpus=$(sysctl -n hw.ncpu)
 
 # Cores
-cpu_cores=$(system_profiler SPHardwareDataType | grep "Cores:" | cut -d: -f2 | sed -Ee 's/^[[:space:]]+//g')
+n_cores=$(system_profiler SPHardwareDataType | grep "Cores:" | cut -d: -f2 | sed -Ee 's/^[[:space:]]+//g')
 
-# CPUS
-cpus=$(sysctl -n hw.ncpu)
+# memory size
+memory_size=$(system_profiler SPHardwareDataType | grep "Memory:" | cut -d: -f2 | sed -Ee 's/^[[:space:]]+//g')
 
 # GPU
 gpu=$(system_profiler SPDisplaysDataType | grep -m1 "Chipset Model" | cut -d: -f2 | sed -Ee 's/^[[:space:]]+//g')
@@ -33,10 +29,13 @@ disk_size=$(diskutil info /dev/disk1 | grep "Disk Size" | cut -d: -f2 | sed -Ee 
 
 echo
 echo "Serial Number: ${serial_number}"
-echo "Processor: ${processor}"
-echo "Memory Size (GB): ${memory_size}"
-echo "Cores: ${cores}"
-echo "CPUS: ${cpus}"
-echo "GPU: ${gpu}"
-echo "Disk Space (GB): ${disk_size}"
+echo "        Model: ${model}"
+echo "         Year: ${year}"
+echo "    Processor: ${processor}"
+echo "         CPUs: ${n_cpus}"
+echo "    CPU Cores: ${n_cores}"
+echo "       Memory: ${memory_gibi} GB"
+echo "Graphics Card: ${gpu}"
+echo "    Disk Size: ${disk_size} GB"
 echo
+
