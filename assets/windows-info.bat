ea96c6d0c2c229b9dda591384f7d2ddc5cb5761b
Leandro Heck <leoheck@gmail.com>
2021-11-09 16:10:05 -0300

    Update

--- assets/windows-info.bat
+++ assets/windows-info.bat
@@ -2,21 +2,21 @@
 
 cls
 
-set owner_name=
-set serial_number=
-set model=
-set year=
-set warranty_expiration=
-set amex_warranty_expiration=
-set processor=
-set n_cpus=
-set n_cores=
-set memory_size=
-set gpu=
-set disk_size=
+rem set owner_name=
+rem set serial_number=
+rem set model=
+rem set year=
+rem set warranty_expiration=
+rem set amex_warranty_expiration=
+rem set processor=
+rem set n_cpus=
+rem set n_cores=
+rem set memory_size=
+rem set gpu=
+rem set disk_size=
 
 rem username
-$username = $env:username -replace "\n" "-"
+$username = $env:username -replace "\n","-"
 $owner_name = (gwmi win32_operatingsystem).RegisteredUser
 
 rem serial number
@@ -28,8 +28,8 @@ $model = (Get-CimInstance -ClassName Win32_ComputerSystem).Model
 rem processor
 $preocessor = (Get-WmiObject -Class Win32_Processor).Name
 
-n_cpus = (gwmi -cl win32_processor).NumberOfLogicalProcessors
-n_cores = (gwmi -cl win32_processor).NumberOfCores
+$n_cpus = (gwmi -cl win32_processor).NumberOfLogicalProcessors
+$n_cores = (gwmi -cl win32_processor).NumberOfCores
 
 rem memory in GB
 $memory_size = (Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum).sum /1gb
@@ -58,8 +58,13 @@ echo                 Disk Size: ${disk_size} GB
 echo
 
 rem # Logfile
-current_date=$(date +"%Y-%m-%d_%Hh%M")
+
+$current_date = $(date +"%Y-%m-%d_%Hh%M")
+
+$current_date = Get-Date -Format "yyyy-MM-dd_HHhmm"
+
 $output_file = "${serial_number}_${current_date}_${username}.csv"
+
 echo "\"${owner_name}\",\"${serial_number}\",\"${model}\",\"${year}\",\"${warranty_expiration}\",\"${amex_warranty_expiration}\",\"${processor}\",\"${n_cpus}\",\"${n_cores}\",\"${memory_size}\",\"${gpu}\",\"${disk_size}\"" > ${output_file}
 
 echo "Output file: $(pwd)\${output_file}"
