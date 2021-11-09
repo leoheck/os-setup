b701d2dfe1090540f25e1c60508cfec4f3f60be9
Leandro Heck <leoheck@gmail.com>
2021-11-09 16:55:21 -0300

    Update

--- assets/windows-info.ps1
+++ assets/windows-info.ps1
@@ -32,7 +32,7 @@ $n_cores = (gwmi -cl win32_processor).NumberOfCores
 $memory_size = (gcim Win32_PhysicalMemory | Measure-Object -Property capacity -Sum).sum /1gb
 
 # gpu
-$gpu = (gcim win32_VideoController).Name[0]
+$gpu = (gwmi win32_VideoController).Name[0]
 
 # disk
 $disk_size = (gcim -cl Win32_LogicalDisk | Select-Object -Property FreeSpace).FreeSpace /1gb -as [int]
@@ -59,7 +59,7 @@ echo ""
 
 $current_date = Get-Date -Format "yyyy-MM_dd_HHhmm"
 $output_file = "${serial_number}_${current_date}_${username}.csv"
-echo "\"${owner_name}\",\"${serial_number}\",\"${model}\",\"${year}\",\"${warranty_expiration}\",\"${amex_warranty_expiration}\",\"${processor}\",\"${n_cpus}\",\"${n_cores}\",\"${memory_size}\",\"${gpu}\",\"${disk_size}\"" > ${output_file}
+echo "'${owner_name}','${serial_number}','${model}','${year}','${warranty_expiration}','${amex_warranty_expiration}','${processor}','${n_cpus}','${n_cores}','${memory_size}','${gpu}','${disk_size}'" > ${output_file}
 
 echo "Output file: $(pwd)\${output_file}"
 echo
