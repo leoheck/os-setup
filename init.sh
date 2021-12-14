fdb40bbc73f0f5f42019ac719cf747a6bc19a92f
Leandro Heck <leoheck@gmail.com>
2021-12-14 20:22:15 -0300

    Update

--- init.sh
+++ init.sh
@@ -15,8 +15,12 @@ sudo chmod -R u+w /usr/local/ &> /dev/null
 # Install and load brew
 yes '' | bash -c "sudo $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ${HOME}/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
+if [[ -f "/opt/homebrew/bin/brew" ]]; then
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ${HOME}/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+else
+    sed -i'' -e "|/opt/homebrew/bin/brew|d" ${HOME}/.zprofile
+fi
 
 # Install required tools
 brew update
