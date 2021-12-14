83856a9d829001ce88ac898e348ef371bde02be2
Leandro Heck <leoheck@gmail.com>
2021-12-14 20:40:03 -0300

    Update

--- userpic.sh
+++ userpic.sh
@@ -6,23 +6,27 @@
 # USAGE:
 # ./userpic.sh USERNAME PICTURE
 
-USERNAME=$1
-USERPIC=$2
+set -e
 
-MAPPINGS='0x0A 0x5C 0x3A 0x2C'
-ATTRS='dsRecTypeStandard:Users 2 dsAttrTypeStandard:RecordName externalbinary:dsAttrTypeStandard:JPEGPhoto'
+declare -x USERNAME="$1"
+declare -x USERPIC="$2"
+
+declare -r DSIMPORT_CMD="/usr/bin/dsimport"
+declare -r ID_CMD="/usr/bin/id"
+
+declare -r MAPPINGS='0x0A 0x5C 0x3A 0x2C'
+declare -r ATTRS='dsRecTypeStandard:Users 2 dsAttrTypeStandard:RecordName externalbinary:dsAttrTypeStandard:JPEGPhoto'
 
 if [ ! -f "${USERPIC}" ]; then
   echo "User image required"
 fi
 
-# Check if the username exists otherwise exit
-id ${USERNAME} &> /dev/null || ( echo "User does not exist" && exit 1 )
+# Check that the username exists - exit on error
+${ID_CMD} "${USERNAME}" &>/dev/null || ( echo "User does not exist" && exit 1 )
 
 declare -r PICIMPORT="$(mktemp /tmp/${USERNAME}_dsimport.XXXXXX)" || exit 1
-
-printf "%s %s \n%s:%s" "${ATTRS}" "${USERNAME}" "${USERPIC}" > "${PICIMPORT}"
-
-dsimport "${PICIMPORT}" /Local/Default M && echo "Successfully imported ${USERPIC} for ${USERNAME}."
+printf "%s %s \n%s:%s" "${MAPPINGS}" "${ATTRS}" "${USERNAME}" "${USERPIC}" >"${PICIMPORT}"
+${DSIMPORT_CMD} "${PICIMPORT}" /Local/Default M &&
+    echo "Successfully imported ${USERPIC} for ${USERNAME}."
 
 rm "${PICIMPORT}"
