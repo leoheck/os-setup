c04c15256511705921bd512087ae571f3398420c
Leandro Heck <leoheck@gmail.com>
2021-12-15 15:55:15 -0300

    Update

--- macos/init.sh
+++ macos/init.sh
@@ -19,7 +19,7 @@ sudo -S <<< ${password} chown -R ${USER} /usr/local/ &> /dev/null
 sudo -S <<< ${password} chmod -R u+w /usr/local/ &> /dev/null
 
 # Install and load brew
-yes '' | bash -c "sudo -S <<< ${password} $(curl -fsSL 'https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh')"
+yes '' | bash -c "sudo -k; sudo -S <<< ${password} $(curl -fsSL 'https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh')"
 
 if [[ -f "/opt/homebrew/bin/brew" ]]; then
     echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ${HOME}/.zprofile
