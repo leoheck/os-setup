61e4d7a46e01d4a08bb76b06c02d00cd5a6d915c
Leandro Heck <leoheck@gmail.com>
2021-12-15 14:12:36 -0300

    Usability improvements

--- macos/init.sh
+++ macos/init.sh
@@ -2,19 +2,16 @@
 
 # Initialize computer with barely minium required stuff to use the command line.
 
-# Asks for sudo password
 echo
-echo "Running sudo, please type password for ${USER}"
-# sudo touch "/tmp/unlock_sudo"
-sudo -k
-echo
+echo "(sudo) Enter password for $USER: "
+echo "$password" | echo "$password" | sudo -S touch /tmp/init_script
 
-# Fix some permissions
-sudo chown -R $(whoami) /usr/local/ &> /dev/null
-sudo chmod -R u+w /usr/local/ &> /dev/null
+# Fix some permissions (for already installed machines)
+echo "$password" | sudo -S chown -R $(whoami) /usr/local/ &> /dev/null
+echo "$password" | sudo -S chmod -R u+w /usr/local/ &> /dev/null
 
 # Install and load brew
-yes '' | bash -c "sudo $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+yes '' | bash -c "echo "$password" | sudo -S $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 if [[ -f "/opt/homebrew/bin/brew" ]]; then
     echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ${HOME}/.zprofile
@@ -34,18 +31,18 @@ brew install dockutil
 if [ ! -d "/Applications/Chat.app" ]
 then
     curl -SsLo ${HOME}/Downloads/GoogleChat.dmg https://dl.google.com/chat/latest/InstallHangoutsChat.dmg
-    sudo hdiutil attach ${HOME}/Downloads/GoogleChat.dmg
-    sudo cp -R "/Volumes/Install Hangouts Chat/Chat.app" /Applications
-    sudo hdiutil unmount "/Volumes/Install Hangouts Chat"
+    echo "$password" | sudo -S hdiutil attach ${HOME}/Downloads/GoogleChat.dmg
+    echo "$password" | sudo -S cp -R "/Volumes/Install Hangouts Chat/Chat.app" /Applications
+    echo "$password" | sudo -S hdiutil unmount "/Volumes/Install Hangouts Chat"
     rm -rf ${HOME}/Downloads/GoogleChat.dmg
 fi
 
 if [ ! -d "/Applications/Google Chrome.app" ]
 then
     curl -SsLo ${HOME}/Downloads/GoogleChrome.dmg https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
-    sudo hdiutil attach ${HOME}/Downloads/GoogleChrome.dmg
-    sudo cp -R "/Volumes/Google Chrome/Google Chrome.app" /Applications
-    sudo hdiutil unmount "/Volumes/Google Chrome"
+    echo "$password" | sudo -S hdiutil attach ${HOME}/Downloads/GoogleChrome.dmg
+    echo "$password" | sudo -S cp -R "/Volumes/Google Chrome/Google Chrome.app" /Applications
+    echo "$password" | sudo -S hdiutil unmount "/Volumes/Google Chrome"
     rm -rf ${HOME}/Downloads/GoogleChrome.dmg
 fi
 
@@ -54,7 +51,7 @@ then
     rm -rf ${HOME}/Downloads/AppCleaner.*
     curl -SsLo "${HOME}/Downloads/AppCleaner.zip" https://freemacsoft.net/downloads/AppCleaner_3.6.zip
     unzip -q AppCleaner.zip
-    sudo mv "${HOME}/Downloads/AppCleaner.app" -f /Applications/
+    echo "$password" | sudo -S mv "${HOME}/Downloads/AppCleaner.app" -f /Applications/
     rm -rf ${HOME}/Downloads/AppCleaner.*
 fi
 
@@ -95,29 +92,29 @@ defaults write com.apple.dock tilesize -integer 48
 killall Dock
 
 # Set hostname with the serial number
-yes | ${HOME}/os-setup/set-hostname.sh
+yes | ${HOME}/os-setup/macos/set-hostname.sh
 
 # Enable Tap to Click
 defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
-sudo defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-sudo defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-sudo defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+echo "$password" | sudo -S defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+echo "$password" | sudo -S defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+echo "$password" | sudo -S defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
 # Set the default picture for Poa Office
 # https://apple.stackexchange.com/questions/117530/setting-account-picture-jpegphoto-with-dscl-in-terminal
-sudo dscl . delete /Users/${USER} JPEGPhoto
-sudo dscl . delete /Users/${USER} Picture
-sudo mkdir -p "/Library/User Pictures/Office/"
-sudo cp -f ${HOME}/os-setup/imgs/poaoffice.tif "/Library/User Pictures/Office/PoaOffice.tif"
-sudo dscl . create /Users/${username} Picture "/Library/User Pictures/Office/PoaOffice.tif"
-sudo ${HOME}/os-setup/userpic.sh ${USER} "/Library/User Pictures/Office/PoaOffice.tif"
+echo "$password" | sudo -S dscl . delete /Users/${USER} JPEGPhoto
+echo "$password" | sudo -S dscl . delete /Users/${USER} Picture
+echo "$password" | sudo -S dscl . create /Users/${username} Picture "/Library/User Pictures/Office/poaoffice.png"
+echo "$password" | sudo -S mkdir -p "/Library/User Pictures/Office/"
+echo "$password" | sudo -S cp -f ${HOME}/os-setup/macos/imgs/poaoffice.tif "/Library/User Pictures/Office/poaoffice.tif"
+echo "$password" | sudo -S ${HOME}/os-setup/macos/userpic.sh ${USER} "/Library/User Pictures/Office/poaoffice.tif"
 
 # (Re)Install OH-MY-ZSH (colors yay!)
 rm -rf ${HOME}/.oh-my-zsh/
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # Update things (hopefully)
-sudo AssetCacheManagerUtil reloadSettings 2> /dev/null
+echo "$password" | sudo -S AssetCacheManagerUtil reloadSettings 2> /dev/null
 
 # Finder > Path Bar
 defaults write com.apple.finder ShowPathbar -bool true
@@ -139,35 +136,35 @@ sed -i "" "s/robbyrussell/spaceship/g" ${HOME}/.zshrc
 
 brew cleanup
 
-# Launch system settings to enable shit that cannot be enabled by script
-echo
-echo
-echo "Turning Remote Login on requires Full Disk Access privileges"
-echo
-echo "To enable the Remote Login go to:"
-echo
-echo "    System Preferences > Security & Privacy > Privacy (tab) > Full Disk Access"
-echo "    And Enable it for Terminal app"
-open -a /System/Applications/System\ Preferences.app &
-echo
-echo
-
 # Disable Guest user
 # Didnt work
-# sudo fdesetup remove -user Guest
+# echo "$password" | sudo -S fdesetup remove -user Guest
 
 # Force system back to English
-choice=$(sudo languagesetup <<< q | grep "English" -m1 | cut -d")" -f1 | sed "s/ //g")
-sudo languagesetup <<< ${choice} &> /dev/null
+choice=$(echo "$password" | sudo -S languagesetup <<< q | grep "English" -m1 | cut -d")" -f1 | sed "s/ //g")
+echo "$password" | sudo -S languagesetup <<< ${choice} &> /dev/null
 
 # Force current keyboard back to English
 find ~/Library/Preferences/ByHost/com.apple.HIToolbox.*
-sudo defaults write ${plist%.*} AppleCurrentKeyboardLayoutInputSourceID -string "com.apple.keylayout.US"
-sudo killall SystemUIServer
+echo "$password" | sudo -S defaults write ${plist%.*} AppleCurrentKeyboardLayoutInputSourceID -string "com.apple.keylayout.US"
+echo "$password" | sudo -S killall SystemUIServer
 
-# Force theme back to white
-# https://apple.stackexchange.com/questions/391686/how-to-set-dark-mode-appearance-to-auto-in-terminal
-# sudo defaults write /Library/Preferences/.GlobalPreferences.plist _HIEnableThemeSwitchHotKey -bool true
+# Force theme clolor to dark
+# https://grrr.tech/posts/2020/switch-dark-mode-os/
+osascript -l JavaScript -e "Application('System Events').appearancePreferences.darkMode = true"
 
 # Finish by going to the scripts folder
 cd ${HOME}/os-setup
+
+# Launch system settings to enable shit that cannot be enabled by script
+# echo
+# echo
+# echo "Turning Remote Login on requires Full Disk Access privileges"
+# echo
+# echo "To enable the Remote Login go to:"
+# echo
+# echo "    System Preferences > Security & Privacy > Privacy (tab) > Full Disk Access"
+# echo "    And Enable it for Terminal app"
+# open -a /System/Applications/System\ Preferences.app &
+# echo
+# echo
