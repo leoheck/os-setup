81d96ec84ba0e723cf5c71809a4b6275f3088427
Leandro Heck <leoheck@gmail.com>
2021-12-15 16:40:43 -0300

    Update

--- macos/init.sh
+++ macos/init.sh
@@ -6,7 +6,7 @@ echo
 read -s -p "(sudo) Enter password for ${USER}: " password
 echo
 sudo -k
-sudo -S <<< ${password} touch /tmp/init_script &> /dev/null
+sudo -S <<< "${password}" touch /tmp/init_script &> /dev/null
 ret=$?
 if [ ! ${ret} -eq 0 ]; then
     echo "Wrong password"
@@ -15,11 +15,16 @@ fi
 
 # Fix some permissions (for already installed machines)
 echo "Fixing permissions..."
-sudo -S <<< ${password} chown -R ${USER} /usr/local/ &> /dev/null
-sudo -S <<< ${password} chmod -R u+w /usr/local/ &> /dev/null
+#sudo -S <<< "${password}" chown -R ${USER} /usr/local/ &> /dev/null
+#sudo -S <<< "${password}" chmod -R u+w /usr/local/ &> /dev/null
 
 # Install and load brew
-bash -c "sudo -S <<< ${password} yes '' |  $(curl -fsSL 'https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh')}"
+
+read -r -d '' cmd << EOM
+sudo -S <<< "${password}" yes '' |  $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)}
+EOM
+
+bash -c "${cmd}"
 
 if [[ -f "/opt/homebrew/bin/brew" ]]; then
     echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ${HOME}/.zprofile
@@ -34,30 +39,30 @@ brew install dockutil
 
 # Install basic usefull software
 
-if [ ! -d "/Applications/Chat.app" ]
+if [ ! -f "/Applications/Chat.app" ]
 then
     curl -SsLo ${HOME}/Downloads/GoogleChat.dmg https://dl.google.com/chat/latest/InstallHangoutsChat.dmg
-    sudo -S <<< ${password} hdiutil attach ${HOME}/Downloads/GoogleChat.dmg
-    sudo -S <<< ${password} cp -R "/Volumes/Install Hangouts Chat/Chat.app" /Applications
-    sudo -S <<< ${password} hdiutil unmount "/Volumes/Install Hangouts Chat"
+    sudo -S <<< "${password}" hdiutil attach ${HOME}/Downloads/GoogleChat.dmg
+    sudo -S <<< "${password}" cp -R "/Volumes/Install Hangouts Chat/Chat.app" /Applications
+    sudo -S <<< "${password}" hdiutil unmount "/Volumes/Install Hangouts Chat"
     rm -rf ${HOME}/Downloads/GoogleChat.dmg
 fi
 
-if [ ! -d "/Applications/Google Chrome.app" ]
+if [ ! -f "/Applications/Google Chrome.app" ]
 then
     curl -SsLo ${HOME}/Downloads/GoogleChrome.dmg https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
-    sudo -S <<< ${password} hdiutil attach ${HOME}/Downloads/GoogleChrome.dmg
-    sudo -S <<< ${password} cp -R "/Volumes/Google Chrome/Google Chrome.app" /Applications
-    sudo -S <<< ${password} hdiutil unmount "/Volumes/Google Chrome"
+    sudo -S <<< "${password}" hdiutil attach ${HOME}/Downloads/GoogleChrome.dmg
+    sudo -S <<< "${password}" cp -R "/Volumes/Google Chrome/Google Chrome.app" /Applications
+    sudo -S <<< "${password}" hdiutil unmount "/Volumes/Google Chrome"
     rm -rf ${HOME}/Downloads/GoogleChrome.dmg
 fi
 
-if [ ! -d "/Applications/AppCleaner.app" ]
+if [ ! -f "/Applications/AppCleaner.app" ]
 then
     rm -rf ${HOME}/Downloads/AppCleaner.*
     curl -SsLo "${HOME}/Downloads/AppCleaner.zip" https://freemacsoft.net/downloads/AppCleaner_3.6.zip
     unzip -q AppCleaner.zip
-    sudo -S <<< ${password} mv "${HOME}/Downloads/AppCleaner.app" -f /Applications/
+    sudo -S <<< "${password}" mv "${HOME}/Downloads/AppCleaner.app" -f /Applications/
     rm -rf ${HOME}/Downloads/AppCleaner.*
 fi
 
@@ -86,41 +91,47 @@ dockutil --remove "Reminders"
 dockutil --remove "TV"
 
 # Add some apps in the dock
-dockutil --add /System/Applications/Utilities/Terminal.app
-dockutil --add /System/Applications/TextEdit.app
-dockutil --add /System/Applications/FindMy.app
-dockutil --add "/Applications/Google Chrome.app"
-dockutil --add "/Applications/Chat.app"
-dockutil --add "/Applications/AppCleaner.app"
+dockutil --add /System/Applications/Utilities/Terminal.app &> /dev/null
+dockutil --add /System/Applications/TextEdit.app  &> /dev/null
+dockutil --add /System/Applications/FindMy.app  &> /dev/null
+dockutil --add "/Applications/Google Chrome.app"  &> /dev/null
+dockutil --add "/Applications/Chat.app"  &> /dev/null
+dockutil --add "/Applications/AppCleaner.app"  &> /dev/null
 
 # Docker size
 defaults write com.apple.dock tilesize -integer 48
 killall Dock
 
 # Set hostname with the serial number
-sudo -S <<< ${password} { yes '' | ${HOME}/os-setup/macos/set-hostname.sh }
+
+
+
+sudo -S <<< "${password}" yes '' | ${HOME}/os-setup/macos/set-hostname.sh
+
+
+
 
 # Enable Tap to Click
 defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
-sudo -S <<< ${password} defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-sudo -S <<< ${password} defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-sudo -S <<< ${password} defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+sudo -S <<< "${password}" defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+sudo -S <<< "${password}" defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+sudo -S <<< "${password}" defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
 # Set the default picture for Poa Office
 # https://apple.stackexchange.com/questions/117530/setting-account-picture-jpegphoto-with-dscl-in-terminal
-sudo -S <<< ${password} dscl . delete /Users/${USER} JPEGPhoto
-sudo -S <<< ${password} dscl . delete /Users/${USER} Picture
-sudo -S <<< ${password} dscl . create /Users/${username} Picture "/Library/User Pictures/Office/poaoffice.png"
-sudo -S <<< ${password} mkdir -p "/Library/User Pictures/Office/"
-sudo -S <<< ${password} cp -f ${HOME}/os-setup/macos/imgs/poaoffice.tif "/Library/User Pictures/Office/poaoffice.tif"
-sudo -S <<< ${password} ${HOME}/os-setup/macos/userpic.sh ${USER} "/Library/User Pictures/Office/poaoffice.tif"
+sudo -S <<< "${password}" dscl . delete /Users/${USER} JPEGPhoto
+sudo -S <<< "${password}" dscl . delete /Users/${USER} Picture
+sudo -S <<< "${password}" dscl . create /Users/${username} Picture "/Library/User Pictures/Office/poaoffice.png"
+sudo -S <<< "${password}" mkdir -p "/Library/User Pictures/Office/"
+sudo -S <<< "${password}" cp -f ${HOME}/os-setup/macos/imgs/poaoffice.tif "/Library/User Pictures/Office/poaoffice.tif"
+sudo -S <<< "${password}" ${HOME}/os-setup/macos/userpic.sh ${USER} "/Library/User Pictures/Office/poaoffice.tif"
 
 # (Re)Install OH-MY-ZSH (colors yay!)
 rm -rf ${HOME}/.oh-my-zsh/
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # Update things (hopefully)
-sudo -S <<< ${password} AssetCacheManagerUtil reloadSettings 2> /dev/null
+sudo -S <<< "${password}" AssetCacheManagerUtil reloadSettings 2> /dev/null
 
 # Finder > Path Bar
 defaults write com.apple.finder ShowPathbar -bool true
@@ -144,16 +155,16 @@ brew cleanup
 
 # Disable Guest user
 # Didnt work
-# sudo -S <<< ${password} fdesetup remove -user Guest
+# sudo -S <<< "${password}" fdesetup remove -user Guest
 
 # Force system back to English
-choice=$(sudo -S <<< ${password} languagesetup <<< q | grep "English" -m1 | cut -d")" -f1 | sed "s/ //g")
-sudo -S <<< ${password} languagesetup <<< ${choice} &> /dev/null
+choice=$(sudo -S <<< "${password}" languagesetup <<< q | grep "English" -m1 | cut -d")" -f1 | sed "s/ //g")
+sudo -S <<< "${password}" languagesetup <<< ${choice} &> /dev/null
 
 # Force current keyboard back to English
 find ~/Library/Preferences/ByHost/com.apple.HIToolbox.*
-sudo -S <<< ${password} defaults write ${plist%.*} AppleCurrentKeyboardLayoutInputSourceID -string "com.apple.keylayout.US"
-sudo -S <<< ${password} killall SystemUIServer
+sudo -S <<< "${password}" defaults write ${plist%.*} AppleCurrentKeyboardLayoutInputSourceID -string "com.apple.keylayout.US"
+sudo -S <<< "${password}" killall SystemUIServer
 
 # Force theme clolor to dark
 # https://grrr.tech/posts/2020/switch-dark-mode-os/
