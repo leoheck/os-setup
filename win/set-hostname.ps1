2c461a43a7af24697e7470da846d76ef277d0e92
Leandro Heck <leoheck@gmail.com>
2022-03-11 15:50:37 -0300

    Update command

--- win/set-hostname.ps1
+++ win/set-hostname.ps1
@@ -13,10 +13,10 @@ if($hostname -eq $null)
 	# Serial Number (Service Tag)
 	echo "Setting hostname as $serial_number ..." 
 	$serial_number = (gwmi win32_bios).SerialNumber
-	Rename-Computer -ComputerName "$serial_number" -NewName "$serial_number" -LocalCredential RemoteComputerAdminUser -Restart
+	Rename-Computer -NewName "$serial_number"
 } 
 else 
 {
 	echo "Setting hostname as $hostname ..." 
-	Rename-Computer -ComputerName "$hostname" -NewName "$hostname" -LocalCredential RemoteComputerAdminUser -Restart
+	Rename-Computer -NewName "$hostname"
 }
