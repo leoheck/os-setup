06ec2dc06eb293aed91fca4d8c9784e952542c19
Leandro Heck <leoheck@gmail.com>
2022-04-18 21:52:08 -0300

    Refactoring

--- macos/init.sh
+++ macos/init.sh
@@ -1,171 +1,217 @@
 #!/bin/bash
 
-export HISTIGNORE='*sudo -S*'
+# Initialize main user
 
-# Initialize computer with barely minium required stuff to use the command line.
+elevate_permissions()
+{
+    export HISTIGNORE='*sudo -S*'
 
-echo
-read -s -p "(sudo) Enter password for ${USER}: " password
-echo
-echo "Installing brew..."
-sudo -S <<< "${password}" touch /tmp/init_script &> /dev/null
+    echo
+    read -s -p "(sudo) Enter password for ${USER}: " password
+    echo
+    sudo -S <<< "${password}" touch /tmp/.init_script &> /dev/null
 
-ret=$?
-if [ ! ${ret} -eq 0 ]; then
-    echo "Wrong password"
-    exit 1
-fi
+    ret=$?
+    if [ ! ${ret} -eq 0 ]; then
+        echo "Wrong password"
+        exit 1
+    fi
+}
 
-# Fix some permissions
-if [[ "${FIX_PERMISSIONS}" != "" ]]; then
-    echo "Fixing permissions..."
-    sudo chown -R ${USER} /usr/local/ &> /dev/null
-    sudo chmod -R u+w /usr/local/ &> /dev/null
-fi
+install_homebrew()
+{
+    # Fix some permissions
+    if [[ "${FIX_PERMISSIONS}" != "" ]]; then
+        echo "Fixing Homebrew permissions..."
+        sudo chown -R ${USER} /usr/local/ &> /dev/null
+        sudo chmod -R u+w /usr/local/ &> /dev/null
+    fi
 
-# Install brew
-# echo "Installing brew..."
-yes '' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-ret=$?
-if [ ! ${ret} -eq 0 ]; then
-    echo "Something went wrong with brew install"
-    exit 1
-fi
+    echo "Installing Homebrew..."
+    yes '' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    ret=$?
+    if [ ! ${ret} -eq 0 ]; then
+        echo "Something went wrong with brew install"
+        exit 1
+    fi
 
-if [[ -f "/opt/homebrew/bin/brew" ]]; then
-    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ${HOME}/.zprofile
-    eval "$(/opt/homebrew/bin/brew shellenv)"
-fi
+    if [[ -f "/opt/homebrew/bin/brew" ]]; then
+        echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ${HOME}/.zprofile
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+    fi
+}
 
-brew update
+install_homebrew_modules()
+{
+    brew install git
+    brew install dockutil
+}
 
-# Install required tools
-brew install git
-brew install dockutil
+install_homebrew_apps()
+{
+    brew install --cask sublime-text  2> /dev/null
+    brew install --cask google-chrome 2> /dev/null
+    brew install --cask brave-browser 2> /dev/null
+    brew install --cask google-chat   2> /dev/null
+    brew install --cask appcleaner    2> /dev/null
+    brew install --cask 1password     2> /dev/null
+}
 
-# Install basic usefull software
-brew install --cask google-chat 2> /dev/null
-brew install --cask sublime-text 2> /dev/null
-brew install --cask google-chrome 2> /dev/null
-brew install --cask appcleaner 2> /dev/null
+reset_hostname()
+{
+    sudo yes '' | "${HOME}/os-setup/macos/set-hostname.sh" &> /dev/null
+}
 
-# Clone os-setup scripts
-cd ~
-rm -rf ${HOME}/os-setup
-git clone https://github.com/leoheck/os-setup.git ${HOME}/os-setup
+configure_login_screen()
+{
+    # Disable guest user
+    sudo sysadminctl -guestAccount off
 
-# Remove garbage from the dock
-dockutil --remove "App Store" 2> /dev/null
-dockutil --remove "Calendar" 2> /dev/null
-dockutil --remove "Contacts" 2> /dev/null
-dockutil --remove "FaceTime" 2> /dev/null
-dockutil --remove "Keynote" 2> /dev/null
-dockutil --remove "Mail" 2> /dev/null
-dockutil --remove "Maps" 2> /dev/null
-dockutil --remove "Messages" 2> /dev/null
-dockutil --remove "Music" 2> /dev/null
-dockutil --remove "News" 2> /dev/null
-dockutil --remove "Notes" 2> /dev/null
-dockutil --remove "Numbers" 2> /dev/null
-dockutil --remove "Pages" 2> /dev/null
-dockutil --remove "Photos" 2> /dev/null
-dockutil --remove "Podcasts" 2> /dev/null
-dockutil --remove "Reminders" 2> /dev/null
-dockutil --remove "TV" 2> /dev/null
-dockutil --remove "TextEdit" 2> /dev/null
+    # Hide poaoffice user from login screen
+    sudo defaults write /Library/Preferences/com.apple.loginwindow HiddenUsersList -array-add ${USER}
 
-# Add some apps in the dock
-dockutil --add /System/Applications/Utilities/Terminal.app 2> /dev/null
-dockutil --add /System/Applications/FindMy.app  2> /dev/null
-dockutil --add "/Applications/Google Chrome.app"  2> /dev/null
-dockutil --add "/Applications/Chat.app"  2> /dev/null
-dockutil --add "/Applications/AppCleaner.app"  2> /dev/null
-dockutil --add "/Applications/Sublime Text.app"  2> /dev/null
+    # Disable "Other.." option on login screen
+    # To open the login prompt:
+    # 1. Tap any arrow key to move focus to the list of accounts (this is not visible)
+    # 2. Then Option+Return, to show the user and password input fields
+    sudo defaults write /Library/Preferences/com.apple.loginwindow SHOWOTHERUSERS_MANAGED -bool FALSE
 
-# Docker size
-defaults write com.apple.dock tilesize -integer 48
-killall Dock
+    login_msg="This computer is property of Ambush"
+    sudo defaults write /Library/Preferences/com.apple.loginwindow LoginwindowText "${login_msg}"
+}
 
-# Set hostname with the serial number
-sudo yes '' | ${HOME}/os-setup/macos/set-hostname.sh &> /dev/null
+clone_repo()
+{
+    rm -rf "${HOME}/os-setup"
+    git clone https://github.com/leoheck/os-setup.git "${HOME}/os-setup"
+}
 
-# Enable Tap to Click
-defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
-sudo defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-sudo defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-sudo defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+set_custom_user_picture()
+{
+    # Set the default picture for Poa Office
+    # https://apple.stackexchange.com/questions/117530/setting-account-picture-jpegphoto-with-dscl-in-terminal
+    sudo dscl . delete "/Users/${USER}" JPEGPhoto
+    sudo dscl . delete "/Users/${USER}" Picture
+    sudo mkdir -p "/Library/User Pictures/Office/"
+    sudo cp -f "${HOME}/os-setup/macos/imgs/poaoffice.tif" "/Library/User Pictures/Office/poaoffice.tif"
+    sudo dscl . create "/Users/${USER}" Picture "/Library/User Pictures/Office/poaoffice.png"
+    sudo "${HOME}/os-setup/macos/userpic.sh" ${USER} "/Library/User Pictures/Office/poaoffice.tif"
+    sudo AssetCacheManagerUtil reloadSettings 2> /dev/null
+}
 
-# Set the default picture for Poa Office
-# https://apple.stackexchange.com/questions/117530/setting-account-picture-jpegphoto-with-dscl-in-terminal
-sudo dscl . delete /Users/${USER} JPEGPhoto
-sudo dscl . delete /Users/${USER} Picture
-sudo dscl . create /Users/${username} Picture "/Library/User Pictures/Office/poaoffice.png"
-sudo mkdir -p "/Library/User Pictures/Office/"
-sudo cp -f ${HOME}/os-setup/macos/imgs/poaoffice.tif "/Library/User Pictures/Office/poaoffice.tif"
-sudo ${HOME}/os-setup/macos/userpic.sh ${USER} "/Library/User Pictures/Office/poaoffice.tif"
+configure_system()
+{
+    # Set system theme dark
+    osascript -l JavaScript -e "Application('System Events').appearancePreferences.darkMode = true"
 
-# (Re)Install OH-MY-ZSH (colors yay!)
-rm -rf ${HOME}/.oh-my-zsh/
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    # Set language to English
+    lang=$(sudo languagesetup <<< q | grep "English" -m1 | cut -d")" -f1 | sed "s/ //g")
+    sudo languagesetup <<< "${lang}" &> /dev/null
 
-# Update things (hopefully)
-sudo AssetCacheManagerUtil reloadSettings 2> /dev/null
+    # Set keyboard to en_US
+    rm -rf ~/Library/Preferences/com.apple.HIToolbox.plist
+    sudo defaults write ${plist%.*} AppleCurrentKeyboardLayoutInputSourceID -string "com.apple.keylayout.US"
+    sudo killall SystemUIServer
 
-# Finder > Path Bar
-defaults write com.apple.finder ShowPathbar -bool true
+    # Enable tap to click
+    sudo defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+    sudo defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+    sudo defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+    sudo defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+}
 
-# Finder > Status Bar
-defaults write com.apple.finder ShowStatusBar -bool true
+configure_finder()
+{
+    # Show path bar on Finder
+    defaults write com.apple.finder ShowPathbar -bool true
 
-# Powerline Fonts
-git clone https://github.com/powerline/fonts.git
-cd fonts
-bash -c ./install.sh
-cd -
-rm -rf fonts
+    # Show status bar on Finder
+    defaults write com.apple.finder ShowStatusBar -bool true
+}
 
-# Zsh Spacehship Theme
-ZSH_CUSTOM="${HOME}/.oh-my-zsh/custom"
-git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-sed -i "" "s/robbyrussell/spaceship/g" ${HOME}/.zshrc
+configure_dock()
+{
+    # Remove garbage from dock
+    dockutil --remove "App Store" 2> /dev/null
+    dockutil --remove "Calendar"  2> /dev/null
+    dockutil --remove "Contacts"  2> /dev/null
+    dockutil --remove "FaceTime"  2> /dev/null
+    dockutil --remove "Keynote"   2> /dev/null
+    dockutil --remove "Mail"      2> /dev/null
+    dockutil --remove "Maps"      2> /dev/null
+    dockutil --remove "Messages"  2> /dev/null
+    dockutil --remove "Music"     2> /dev/null
+    dockutil --remove "News"      2> /dev/null
+    dockutil --remove "Notes"     2> /dev/null
+    dockutil --remove "Numbers"   2> /dev/null
+    dockutil --remove "Pages"     2> /dev/null
+    dockutil --remove "Photos"    2> /dev/null
+    dockutil --remove "Podcasts"  2> /dev/null
+    dockutil --remove "Reminders" 2> /dev/null
+    dockutil --remove "TextEdit"  2> /dev/null
+    dockutil --remove "TV"        2> /dev/null
 
-brew cleanup
+    # Add some apps in dock
+    dockutil --add "/System/Applications/Utilities/Terminal.app" 2> /dev/null
+    dockutil --add "/System/Applications/FindMy.app" 2> /dev/null
+    dockutil --add "/Applications/Google Chrome.app" 2> /dev/null
+    dockutil --add "/Applications/Chat.app" 2> /dev/null
+    dockutil --add "/Applications/Sublime Text.app" 2> /dev/null
 
-# Disable Guest user
-# Didnt work
-# sudo fdesetup remove -user Guest
+    # Set docker size smaller
+    defaults write com.apple.dock tilesize -integer 48
+    killall Dock
+}
 
-# Force system back to English
-choice=$(sudo languagesetup <<< q | grep "English" -m1 | cut -d")" -f1 | sed "s/ //g")
-sudo languagesetup <<< ${choice} &> /dev/null
+configure_terminal()
+{
+    # Install terminal powerline fonts
+    git clone https://github.com/powerline/fonts.git
+    cd fonts
+    bash -c ./install.sh
+    cd -
+    rm -rf fonts
 
-# Force current keyboard back to English
-find /Library/Preferences/com.apple.HIToolbox.plist
-sudo defaults write ${plist%.*} AppleCurrentKeyboardLayoutInputSourceID -string "com.apple.keylayout.US"
-sudo killall SystemUIServer
+    # Install OH-MY-ZSH
+    rm -rf "${HOME}/.oh-my-zsh/"
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-# Force theme clolor to dark
-# https://grrr.tech/posts/2020/switch-dark-mode-os/
-osascript -l JavaScript -e "Application('System Events').appearancePreferences.darkMode = true"
+    # Set zsh with SpaceShip
+    ZSH_CUSTOM_THEMES="${HOME}/.oh-my-zsh/custom/themes/"
+    mkdir -p ${ZSH_CUSTOM_THEMES}
+    git clone https://github.com/spaceship-prompt/spaceship-prompt.git "${ZSH_CUSTOM_THEMES}/spaceship-prompt" --depth=1
+    ln -s "${ZSH_CUSTOM_THEMES}/spaceship-prompt/spaceship.zsh-theme" "${ZSH_CUSTOM_THEMES}/spaceship.zsh-theme"
+    sed -i "" "s/robbyrussell/spaceship/g" ${HOME}/.zshrc
+}
 
-# Finish by going to the scripts folder
-cd ${HOME}/os-setup
+collect_computer_info()
+{
+    # Refresh database with computers info
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/leoheck/os-setup/main/assets/macos-info.sh)"
+}
 
-# Refresh database with computers info
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/leoheck/os-setup/main/assets/macos-info.sh)"
+customize_current_user()
+{
+    set_custom_user_picture
+    configure_system
+    configure_finder
+    configure_dock
+    configure_terminal
+}
 
-# Disable guest user
-sudo sysadminctl -guestAccount off
+main()
+{
+    elevate_permissions
+    install_homebrew
+    brew update
+    install_homebrew_modules
+    install_homebrew_apps
+    brew cleanup
+    clone_repo
+    reset_hostname
+    configure_login_screen
+    customize_current_user
+    collect_computer_info
+}
 
-# Hide poa.office user from login screen
-sudo chflags hidden /Users/${USER}
-
-# Disable "Other..." on login screen
-# Hold down SHift+Option+Down Arrow+Return to reveal the username and password field at the login screen.
-# The actual procedure is to tap any arrow key to move focus to the list of accounts, then option-return — not holding shift which is for disabling login items and not holding down arrow.
-sudo defaults write /Library/Preferences/com.apple.loginwindow SHOWOTHERUSERS_MANAGED -bool FALSE
-
-# Login screen mesasge
-login_message="This computer is property of Ambush"
-sudo defaults write /Library/Preferences/com.apple.loginwindow LoginwindowText "${login_message}"
+main "$@"
